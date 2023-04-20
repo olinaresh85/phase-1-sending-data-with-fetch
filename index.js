d461c4e469bcf8040059ce6dd9759c44b17d314a
Naresh Oli <olinaresh85@gmail.com>
2023-04-20 00:28:05 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name , email})
+
+    })
+    .then(res => res.json())
+    .then(object => {
+        console.log(object);
+        const h1 = document.createElement('h1');
+        const body = document.querySelector('body');
+        body.append(h1);
+        h1.textContent = `${object.id}`;
+    
+    })
+    .catch(error => {
+        
+        // console.log(error);
+        // alert('Unuthorized Access');
+        const h2 = document.createElement('h1');
+        const body = document.querySelector('body');
+        body.append(h2);
+        h2.textContent=(error.message);
+    });
+};
+
+
+
+submitData();
